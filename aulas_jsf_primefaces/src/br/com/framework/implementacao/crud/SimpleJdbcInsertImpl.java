241a8f857dbd7878a11de26b11a864e3b5c3e2ed
Brauner <Brauner@DESKTOP-C5KLHJ6>
2019-01-19 23:29:05 -0300

    Alterei a classe (SimpleJdbcInsertImpl).

--- aulas_jsf_primefaces/src/br/com/framework/implementacao/crud/SimpleJdbcInsertImpl.java
+++ aulas_jsf_primefaces/src/br/com/framework/implementacao/crud/SimpleJdbcInsertImpl.java
@@ -2,10 +2,16 @@ package br.com.framework.implementacao.crud;
 
 import java.io.Serializable;
 
+import javax.sql.DataSource;
+
 import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
 
 public class SimpleJdbcInsertImpl extends SimpleJdbcInsert implements Serializable {
  
+	public SimpleJdbcInsertImpl(DataSource dataSource) {
+		super(dataSource);
+	}
+
 	private static final long serialVersionUID = 1L;
 	
 }
