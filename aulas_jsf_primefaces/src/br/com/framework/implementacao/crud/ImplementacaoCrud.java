4c9913a46271d82e8484f6ae6d5eff608cc6169b
Brauner <Brauner@DESKTOP-C5KLHJ6>
2019-01-24 23:47:11 -0300

    Implementacao CRUD Parte 4

--- aulas_jsf_primefaces/src/br/com/framework/implementacao/crud/ImplementacaoCrud.java
+++ aulas_jsf_primefaces/src/br/com/framework/implementacao/crud/ImplementacaoCrud.java
@@ -1,6 +1,8 @@
 package br.com.framework.implementacao.crud;
 
+import java.util.ArrayList;
 import java.util.List;
+
 import org.hibernate.Query;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
@@ -19,144 +21,208 @@ import br.com.framework.interfac.crud.InterfaceCrud;
 public class ImplementacaoCrud<T> implements InterfaceCrud<T> {
 
 	private static final long serialVersionUID = 1L;
-	
+
 	private static SessionFactory sessionFactory = HibernateUtil.getSessionFactory();
-	
+
 	@Autowired
 	private JdbcTemplateImpl JdbcTemplate;
-	
-	// Aqui!	
-	
+
+	@Autowired
+	private SimpleJdbcTemplateImpl simpleJdbcTemplate;
+
+	@Autowired
+	private SimpleJdbcInsertImpl simpleJdbcInsert;
+
+	@Autowired
+	private SimpleJdbcTemplateImpl simpleJdbcTemplateImpl;
+
+	public SimpleJdbcTemplate getSimpleJdbcTemplateImpl() {
+		return simpleJdbcTemplateImpl;
+	}
+
 	@Override
 	public void save(T obj) throws Exception {
-		// TODO Auto-generated method stub
-
+		validaSessionFactory();
+		sessionFactory.getCurrentSession().save(obj);
+		executeFlushSession();
 	}
 
 	@Override
 	public void persist(T obj) throws Exception {
-		// TODO Auto-generated method stub
-
+		validaSessionFactory();
+		sessionFactory.getCurrentSession().persist(obj);
+		executeFlushSession();
 	}
 
 	@Override
 	public void saveOrUpdate(T obj) throws Exception {
-		// TODO Auto-generated method stub
-
+		validaSessionFactory();
+		sessionFactory.getCurrentSession().saveOrUpdate(obj);
+		executeFlushSession();
 	}
 
 	@Override
 	public void update(T obj) throws Exception {
-		// TODO Auto-generated method stub
-
+		validaSessionFactory();
+		sessionFactory.getCurrentSession().save(obj);
+		executeFlushSession();
 	}
 
 	@Override
 	public void delete(T obj) throws Exception {
-		// TODO Auto-generated method stub
-
+		validaSessionFactory();
+		sessionFactory.getCurrentSession().delete(obj);
+		executeFlushSession();
 	}
 
 	@Override
 	public T merge(T obj) throws Exception {
-		// TODO Auto-generated method stub
-		return null;
+		validaSessionFactory();
+		obj = (T) sessionFactory.getCurrentSession().merge(obj);
+		executeFlushSession();
+		return obj;
 	}
 
 	@Override
-	public List<T> findList(Class<T> obj) throws Exception {
-		// TODO Auto-generated method stub
-		return null;
+	public List<T> findList(Class<T> entidade) throws Exception {
+		validaSessionFactory();
+		StringBuilder query = new StringBuilder();
+		query.append(" select distinct(entity) from ").append(entidade.getSimpleName()).append(" entity ");
+
+		List<T> lista = sessionFactory.getCurrentSession().createQuery(query.toString()).list();
+
+		return lista;
 	}
 
 	@Override
 	public Object findById(Class<T> entidade, Long id) throws Exception {
-		// TODO Auto-generated method stub
-		return null;
+		validaSessionFactory();
+		Object obj = sessionFactory.getCurrentSession().load(getClass(), id);
+		return obj;
 	}
 
 	@Override
 	public T findByPorId(Class<T> entidade, Long id) throws Exception {
-		// TODO Auto-generated method stub
-		return null;
+		validaSessionFactory();
+		T obj = (T) sessionFactory.getCurrentSession().load(getClass(), id);
+		return obj;
 	}
 
 	@Override
 	public List<T> findListByQueryDinamica(String s) throws Exception {
-		// TODO Auto-generated method stub
-		return null;
+		validaSessionFactory();
+		List<T> lista = new ArrayList<T>();
+		lista = sessionFactory.getCurrentSession().createQuery(s).list();
+		return lista;
 	}
 
 	@Override
 	public void executeUpdateQueryDinamica(String s) throws Exception {
-		// TODO Auto-generated method stub
-
+		validaSessionFactory();
+		sessionFactory.getCurrentSession().createQuery(s).executeUpdate();
+		executeFlushSession();
 	}
 
 	@Override
 	public void executeUpdateSQLDinamica(String s) throws Exception {
-		// TODO Auto-generated method stub
-
+		validaSessionFactory();
+		sessionFactory.getCurrentSession().createSQLQuery(s).executeUpdate();
+		executeFlushSession();
 	}
 
 	@Override
 	public void clearSession() throws Exception {
-		// TODO Auto-generated method stub
-
+		sessionFactory.getCurrentSession().clear();
 	}
 
 	@Override
 	public void evict(Object objs) throws Exception {
-		// TODO Auto-generated method stub
-
+		validaSessionFactory();
+		sessionFactory.getCurrentSession().evict(objs);
 	}
 
 	@Override
 	public Session getSession() throws Exception {
-		// TODO Auto-generated method stub
-		return null;
+		validaSessionFactory();
+		return sessionFactory.getCurrentSession();
 	}
 
 	@Override
 	public List<?> getListSQLDinamica(String sql) throws Exception {
-		// TODO Auto-generated method stub
-		return null;
+		validaSessionFactory();
+		List<?> lista = sessionFactory.getCurrentSession().createSQLQuery(sql).list();
+		return lista;
 	}
 
 	@Override
 	public JdbcTemplate getJdbcTemplate() {
-		// TODO Auto-generated method stub
-		return null;
+		return JdbcTemplate;
 	}
 
 	@Override
 	public SimpleJdbcTemplate getSimpleJdbcTemplate() {
-		// TODO Auto-generated method stub
-		return null;
+		return simpleJdbcTemplate;
 	}
 
 	@Override
 	public SimpleJdbcInsert getSimpleJdbcInsert() {
-		// TODO Auto-generated method stub
-		return null;
+		return simpleJdbcInsert;
 	}
 
 	@Override
 	public Long totalRegistro(String table) throws Exception {
-		// TODO Auto-generated method stub
-		return null;
+		StringBuilder sql = new StringBuilder();
+		sql.append(" select count(1) from ").append(table);
+		return JdbcTemplate.queryForLong(sql.toString());
 	}
 
 	@Override
 	public Query obterQuery(String query) throws Exception {
-		// TODO Auto-generated method stub
-		return null;
+		validaSessionFactory();
+		Query queryReturn = sessionFactory.getCurrentSession().createQuery(query.toString());
+		return queryReturn;
 	}
 
 	@Override
 	public List<T> findListByQueryDinamica(String query, int iniciaNoRegistro, int maximoResultado) throws Exception {
-		// TODO Auto-generated method stub
-		return null;
+		validaSessionFactory();
+		List<T> lista = new ArrayList<T>();
+		lista = sessionFactory.getCurrentSession().createQuery(query).setFirstResult(iniciaNoRegistro).setMaxResults(maximoResultado).list();
+		return lista;
+	}
+
+	private void validaSessionFactory() {
+		if (sessionFactory == null) {
+			sessionFactory = HibernateUtil.getSessionFactory();
+		}
+		validaTransaction();
+	}
+
+	private void validaTransaction() {
+		if (!sessionFactory.getCurrentSession().getTransaction().isActive()) {
+			sessionFactory.getCurrentSession().beginTransaction();
+		}
+	}
+
+	private void commitProcessoAjax() {
+		sessionFactory.getCurrentSession().beginTransaction().commit();
+	}
+
+	private void rollBackProcessoAjax() {
+		sessionFactory.getCurrentSession().beginTransaction().rollback();
+	}
+
+	/**
+	 * Roda instantaneamente o SQL do banco de dados
+	 */
+	private void executeFlushSession() {
+		sessionFactory.getCurrentSession().flush();
+	}
+
+	public List<Object[]> getListSQLDinamicaArray(String sql) throws Exception {
+		validaSessionFactory();
+		List<Object[]> lista = (List<Object[]>) sessionFactory.getCurrentSession().createQuery(sql).list();
+		return lista;
 	}
 
 }
--- aulas_jsf_primefaces/src/br/com/framework/implementacao/crud/SimpleJdbcInsertImpl.java
+++ aulas_jsf_primefaces/src/br/com/framework/implementacao/crud/SimpleJdbcInsertImpl.java
@@ -5,13 +5,17 @@ import java.io.Serializable;
 import javax.sql.DataSource;
 
 import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
 
+@Component
+@Transactional(propagation = Propagation.REQUIRED, rollbackFor = Exception.class)
 public class SimpleJdbcInsertImpl extends SimpleJdbcInsert implements Serializable {
- 
+	
+	private static final long serialVersionUID = 1L;
+	
 	public SimpleJdbcInsertImpl(DataSource dataSource) {
 		super(dataSource);
 	}
-
-	private static final long serialVersionUID = 1L;
-	
 }
